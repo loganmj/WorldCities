22eca6ec4dfbc79ddbec2924366dd7759f1005f2
Michael <michaellogan7860@gmail.com>
2024-09-24 12:15:53 -0500

    Generated HomeComponent and NavMenuComponent.

--- worldcities.client/src/app/app.module.ts
+++ worldcities.client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { NavMenuComponentComponent } from './nav-menu-component/nav-menu-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    NavMenuComponentComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ worldcities.client/src/app/nav-menu-component/nav-menu-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu-component',
+  templateUrl: './nav-menu-component.component.html',
+  styleUrl: './nav-menu-component.component.css'
+})
+export class NavMenuComponentComponent {
+
+}
