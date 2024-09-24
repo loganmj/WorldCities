8c076f91a286d6757c8f44db5f3588190673d4c4
Michael <michaellogan7860@gmail.com>
2024-09-24 12:25:36 -0500

    Removed weather forecast features from app. Deleted app.component.spec.ts.

--- worldcities.client/src/app/app.module.ts
+++ worldcities.client/src/app/app.module.ts
@@ -1,24 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { NavMenuComponentComponent } from './nav-menu-component/nav-menu-component.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
-    NavMenuComponentComponent,
     HomeComponent,
     NavMenuComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule, HttpClient,
     AppRoutingModule
   ],
   providers: [],
