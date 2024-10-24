2d7bea33cfce819727b3379fcf226ab19cc64201
Michael <michaellogan7860@gmail.com>
2024-10-24 14:31:53 -0500

    Added dbcontext class and secrets file.

---
+++ WorldCities.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Data.Models;
+
+namespace WorldCities.Server.Data;
+
+/// <summary>
+/// Database context
+/// </summary>
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext() : base() { }
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<Country> Countries => Set<Country>();
+}
