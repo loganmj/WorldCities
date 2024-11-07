74d72b2fcc539e18191670bf0d79a009f96318d5
Michael <michaellogan7860@gmail.com>
2024-11-07 12:02:34 -0600

    Installed the EPPlus package for importing Excel files. Added the Excel seed file into Server/Data/Source.

---
+++ WorldCities.Server/Controllers/SeedController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorldCities.Server.Data;
+
+namespace WorldCities.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController : ControllerBase
+    {
+        #region Fields
+
+        private readonly ApplicationDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a controller with a give DB context.
+        /// </summary>
+        /// <param name="context"></param>
+        public SeedController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+    }
+}
