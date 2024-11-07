3a792bfd9af4a3549888131531a6cc4a7078a470
Michael <michaellogan7860@gmail.com>
2024-11-07 14:32:56 -0600

    Generated an entity controller for Cities.

---
+++ WorldCities.Server/Controllers/CitiesController.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Data;
+using WorldCities.Server.Data.Models;
+
+namespace WorldCities.Server.Controllers
+{
+    /// <summary>
+    /// An API controller for city data
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        #region Fields
+
+        private readonly ApplicationDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a controller with the specified database context.
+        /// </summary>
+        /// <param name="context"></param>
+        public CitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Checks if a city exists in the database.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private bool CityExists(int id)
+        {
+            return _context.Cities.Any(e => e.ID == id);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Gets a list of all cities from the database.
+        /// </summary>
+        /// <returns>An IEnumerable containing the City objects.</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await _context.Cities.ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets a city with the specified ID from the database.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A City object.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCity(int id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return city;
+        }
+
+        /// <summary>
+        /// Sends a city to the API controller.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        /// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(int id, City city)
+        {
+            if (id != city.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Adds a city to the database.
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            _context.Cities.Add(city);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.ID }, city);
+        }
+
+        /// <summary>
+        /// Deletes a city with the specified ID from the database.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cities.Remove(city);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        #endregion
+    }
+}
