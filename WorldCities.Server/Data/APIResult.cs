ce423b39475ca1862084a3775e09ce389c89f0f6
Michael <michaellogan7860@gmail.com>
2024-11-15 11:19:20 -0600

    Changed PaginatedResult to APIResult.

--- WorldCities.Server/Data/APIResult.cs
+++ WorldCities.Server/Data/APIResult.cs
@@ -5,7 +5,7 @@ namespace WorldCities.Server.Data
     /// <summary>
     /// A custom API result class that allows us to return additional information about our dataset.
     /// </summary>
-    public class PaginatedResult<T>
+    public class APIResult<T>
     {
         #region Properties
 
@@ -55,7 +55,7 @@ namespace WorldCities.Server.Data
         /// <param name="count"></param>
         /// <param name="pageIndex"></param>
         /// <param name="pageSize"></param>
-        private PaginatedResult(List<T> data, int count, int pageIndex, int pageSize)
+        private APIResult(List<T> data, int count, int pageIndex, int pageSize)
         {
             Data = data;
             PageIndex = pageIndex;
@@ -75,7 +75,7 @@ namespace WorldCities.Server.Data
         /// <param name="pageIndex">The current page index.</param>
         /// <param name="pageSize">The size of each page.</param>
         /// <returns> An APIResult object containing the paged result and all of the relevant paging navigation information.</returns>
-        public static async Task<PaginatedResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        public static async Task<APIResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             // Gets the total number of elements in the source
             var count = await source.CountAsync();
@@ -85,7 +85,7 @@ namespace WorldCities.Server.Data
             var data = await source.ToListAsync();
 
             // Return the paginated results, and additional data
-            return new PaginatedResult<T>(data, count, pageIndex, pageSize);
+            return new APIResult<T>(data, count, pageIndex, pageSize);
         }
 
         #endregion
