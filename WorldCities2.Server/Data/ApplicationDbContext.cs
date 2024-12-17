42711c8cf7f3572a21c1957fcc675b03732f36ca
Michael <michaellogan7860@gmail.com>
2024-12-17 15:23:10 -0600

    Added db connection string.

--- WorldCities2.Server/Data/ApplicationDbContext.cs
+++ WorldCities2.Server/Data/ApplicationDbContext.cs
@@ -1,6 +1,39 @@
-﻿namespace WorldCities2.Server.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities2.Server.Data.Models;
+
+namespace WorldCities.Server.Data;
+
+/// <summary>
+/// Database context
+/// </summary>
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext
-    {
-    }
+    #region Constructors
+
+    /// <summary>
+    /// Parameterless constructor.
+    /// </summary>
+    public ApplicationDbContext() : base() { }
+
+    /// <summary>
+    /// Copy constructor.
+    /// </summary>
+    /// <param name="options"></param>
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Accessor for a City object.
+    /// </summary>
+    public DbSet<City> Cities => Set<City>();
+
+    /// <summary>
+    /// Accessor for a Country object.
+    /// </summary>
+    public DbSet<Country> Countries => Set<Country>();
+
+    #endregion
 }
--- WorldCities2.Server/Program.cs
+++ WorldCities2.Server/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add configuration to read from user secrets
+builder.Configuration.AddUserSecrets<Program>();
+
 // Add services to the container.
+// Add JSON options so that indentation is supproted on all browsers.
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
+// Add ApplicationDbContext and MySQL Server support
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
