65464c211d3730e200868c9b40f8312a8e9057f6
Michael <michaellogan7860@gmail.com>
2024-12-17 12:40:11 -0600

    Generated the HomeComponent and NavMenuComponent.

---
+++ worldcities2.client/src/app/nav-menu-component/nav-menu-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu-component',
+  standalone: false,
+  
+  templateUrl: './nav-menu-component.component.html',
+  styleUrl: './nav-menu-component.component.css'
+})
+export class NavMenuComponentComponent {
+
+}
