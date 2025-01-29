20b759c70f53f87369fe42fa7c309ba2cce6eb9c
Michael <michaellogan7860@gmail.com>
2025-01-29 14:14:09 -0600

    Added properties to the countryDTO object.

--- WorldCities2.Server/Data/Models/CountryDTO.cs
+++ WorldCities2.Server/Data/Models/CountryDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace WorldCities2.Server.Data.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WorldCities2.Server.Data.Models
 {
     /// <summary>
     /// A country data transfer object that carries the desired country data to the front end.
@@ -6,6 +8,34 @@
     public class CountryDTO
     {
         #region Properties
+
+        /// <summary>
+        /// The unique id and primary key for this country.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The country name (in UTF8 format).
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The country code (in ISO 3166-1 ALPHA-2 format).
+        /// </summary>
+        [JsonPropertyName("iso2")]
+        public required string ISO2 { get; set; }
+
+        /// <summary>
+        /// The country code (in ISO 3166-1 ALPHA-3 format).
+        /// </summary>
+        [JsonPropertyName("iso3")]
+        public required string ISO3 { get; set; }
+
+        /// <summary>
+        /// The number of cities listed for the country.
+        /// </summary>
+        public int? NumberOfCities { get; set; } = null;
+
         #endregion
     }
 }
