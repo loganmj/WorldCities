4d9a653b44fd50b2ea929702a476c0e7c6c0a746
Michael <michaellogan7860@gmail.com>
2025-01-29 14:07:52 -0600

    Added CountryDTO class.

---
+++ WorldCities2.Server/Data/Models/CountryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WorldCities2.Server.Data.Models
+{
+    /// <summary>
+    /// A country data transfer object that carries the desired country data to the front end.
+    /// </summary>
+    public class CountryDTO
+    {
+        #region Properties
+        #endregion
+    }
+}
