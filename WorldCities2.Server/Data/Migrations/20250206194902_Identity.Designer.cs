42941d5810201c5688f1023e08ab6dd636af3ec2
Michael <michaellogan7860@gmail.com>
2025-02-06 13:52:06 -0600

    Updated the database using MySQL migration.

--- WorldCities2.Server/Data/Migrations/20250206194902_Identity.Designer.cs
+++ WorldCities2.Server/Data/Migrations/20250206194902_Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorldCities2.Server.Data;
 
@@ -11,9 +12,11 @@ using WorldCities2.Server.Data;
 namespace WorldCities2.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250206194902_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
