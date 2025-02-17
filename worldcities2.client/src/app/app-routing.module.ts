84503735c1aa3f8ee35a4a625442d5e486ec8e4c
Michael <michaellogan7860@gmail.com>
2025-02-17 11:01:41 -0600

    Added canActivate guard to city and country edit routes.

--- worldcities2.client/src/app/app-routing.module.ts
+++ worldcities2.client/src/app/app-routing.module.ts
@@ -6,15 +6,16 @@ import { CountriesComponent } from './countries/countries.component';
 import { CityEditComponent } from './cities/city-edit.component';
 import { CountryEditComponent } from './countries/country-edit.component';
 import { LoginComponent } from './auth/login.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'cities', component: CitiesComponent },
   { path: 'countries', component: CountriesComponent },
-  { path: 'city/:id', component: CityEditComponent },
-  { path: 'city', component: CityEditComponent },
-  { path: 'country/:id', component: CountryEditComponent },
-  { path: 'country', component: CountryEditComponent },
+  { path: 'city/:id', component: CityEditComponent, canActivate: [AuthGuard] },
+  { path: 'city', component: CityEditComponent, canActivate: [AuthGuard] },
+  { path: 'country/:id', component: CountryEditComponent, canActivate: [AuthGuard] },
+  { path: 'country', component: CountryEditComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
