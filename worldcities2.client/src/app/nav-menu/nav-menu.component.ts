cfdc0025c014aebfbea7be7bd20ff5d574443b56
Michael <michaellogan7860@gmail.com>
2025-02-17 08:30:34 -0600

    Added hook to nav menu so that the login button watches the isLoggedIn property to function.

--- worldcities2.client/src/app/nav-menu/nav-menu.component.ts
+++ worldcities2.client/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,6 +9,62 @@ import { Component } from '@angular/core';
   styleUrl: './nav-menu.component.scss',
   standalone: false
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit, OnDestroy{
 
+  // #region Fields
+
+  private _destroySubject = new Subject();
+
+  // #endregion
+
+  // #region Properties
+
+  /**
+   * Is the user logged in.
+   */ 
+  public isLoggedIn: boolean = false;
+
+  // #endregion
+
+  // #region Constructors
+
+  /**
+   * Constructor
+   */ 
+  public constructor(private authService: AuthService, private router: Router) {
+    this.authService.authStatus
+      .pipe(takeUntil(this._destroySubject))
+      .subscribe(result => {
+        this.isLoggedIn = result;
+      })
+  }
+
+  // #endregion
+
+  // #region Public Methods
+
+  /**
+   * Handles a user logout.
+   */ 
+  public onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(["/"]);
+  }
+
+  /**
+   * On init.
+   */ 
+  public ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  /**
+   * On destroy
+   */ 
+  public ngOnDestroy(): void {
+    this._destroySubject.next(true);
+    this._destroySubject.complete();
+  }
+
+  // #endregion
 }
