c66ddaf8e4d204224b76cef7001ad2e21d062c93
Michael <michaellogan7860@gmail.com>
2025-02-27 08:38:35 -0600

    Added GraphQL Query and Mutation classes.

---
+++ WorldCities2.Server/Data/GraphQL/Mutation.cs
@@ -0,0 +1,20 @@
+﻿namespace WorldCities2.Server.Data.GraphQL
+{
+    /// <summary>
+    /// Implements a GraphQL mutation. This allows us to write to our database.
+    /// </summary>
+    public class Mutation
+    {
+        #region Public Methods
+
+        // TODO: Add a new city
+        // TODO: Update an existing city
+        // TODO: Delete a city
+
+        // TODO: Add a new country
+        // TODO: Update an existing country
+        // TODO: Delete a country
+
+        #endregion
+    }
+}
---
+++ WorldCities2.Server/Data/GraphQL/Query.cs
@@ -0,0 +1,42 @@
+﻿using WorldCities2.Server.Data.Models;
+
+namespace WorldCities2.Server.Data.GraphQL
+{
+    /// <summary>
+    /// Represents a GraphQL query. This class allows for reading data from our DB.
+    /// </summary>
+    public class Query
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Gets all cities from the datatbase.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        [Serial]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<City> GetCities([Service] ApplicationDbContext context)
+        {
+            return context.Cities;
+        }
+
+        /// <summary>
+        /// Gets all countries from the database.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        [Serial]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Country> GetCountries([Service] ApplicationDbContext context)
+        {
+            return context.Countries;
+        }
+
+        #endregion
+    }
+}
